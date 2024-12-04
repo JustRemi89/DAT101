a2ab494fe690ca8bf8c75ca093fe2b6f32a08a1e
JustRemi89 <remi-johansen@outlook.com>
2024-12-04 11:11:36 +0100

    Fixed part 8

--- module_4/task_4-2/task_4-2.mjs
+++ module_4/task_4-2/task_4-2.mjs
@@ -212,11 +212,11 @@ printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut(randomNumbers.join(", "));
-const ascendingNumbers = randomNumbers.sort((a, b) => a - b);
-printOut("Ascending:: " + ascendingNumbers.join(", "));
-const descendingNumbers = randomNumbers.sort((a, b) => b - a);
-printOut("Descending: " + descendingNumbers.join(", "));
+printOut("Tabellen i tilfeldig rekkefølge: " + randomNumbers.join(", "));
+randomNumbers.sort(sortRandomValues);
+printOut("Tabellen i stigende rekkefølge: " + randomNumbers.join(", "));
+randomNumbers.reverse();
+printOut("Tabellen i synkende rekkefølge: " + randomNumbers.join(", "));
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");

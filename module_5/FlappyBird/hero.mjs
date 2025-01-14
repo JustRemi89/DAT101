d62895a0a39609487b6bbee10c160bf2af80f568
JustRemi89 <remi-johansen@outlook.com>
2025-01-14 12:23:36 +0100

    created THero and export default

--- module_5/FlappyBird/hero.mjs
+++ module_5/FlappyBird/hero.mjs
@@ -1,1 +1,14 @@
 "use strict";
+import libSprite from "../../common/libs/libSprite.mjs";
+
+class THero extends libSprite.TSprite {
+    constructor(aSpriteCanvas, aSpriteInfo, aPosition) {
+        super(aSpriteCanvas, aSpriteInfo, aPosition);
+    }
+
+    draw() {
+        super.draw();
+    }
+}
+
+export default THero;

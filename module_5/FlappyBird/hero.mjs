0c02a459e30880b4aba566109946dc5217078fca
JustRemi89 <remi-johansen@outlook.com>
2025-01-14 13:11:34 +0100

    added gravity and velocity

--- module_5/FlappyBird/hero.mjs
+++ module_5/FlappyBird/hero.mjs
@@ -3,8 +3,12 @@ import libSprite from "../../common/libs/libSprite.mjs";
 import { GameProps, SpriteInfoList } from "./FlappyBird.mjs";
 
 class THero extends libSprite.TSprite {
+    #spi;
+    #gravity = 9.81 / 100;
+    #velocity = 0;
     constructor(aSpriteCanvas, aSpriteInfo, aPosition) {
         super(aSpriteCanvas, aSpriteInfo, aPosition);
+        this.#spi = aSpriteInfo;
         this.animateSpeed = 10; // Max 99
     }
 
@@ -13,12 +17,13 @@ class THero extends libSprite.TSprite {
     }
 
     update() {
-        const groundPosY = GameProps.ground.posY;
-        const maxFall = groundPosY - SpriteInfoList.hero1.height;
-        if(this.posY < maxFall) {
+        const groundY = GameProps.ground.posY;
+        const bottomY = this.posY + this.#spi.height;
+        if(bottomY < groundY) {
             this.translate(0, 1);
+            this.#velocity += this.#gravity;
         } else {
-            this.posY = maxFall;
+            this.posY = groundY - this.#spi.height;
         }
     }
 }

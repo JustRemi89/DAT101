5db1c2f25bdb11062adc9e051758509bfc7c5ac6
JustRemi89 <remi-johansen@outlook.com>
2025-01-21 14:00:30 +0100

    bait now goes up and down in a randomly fashion

--- module_5/FlappyBird/bait.mjs
+++ module_5/FlappyBird/bait.mjs
@@ -1,5 +1,6 @@
 "use strict";
 import libSprite from "../../common/libs/libSprite.mjs";
+import lib2d from "../../common/libs/lib2d.mjs";
 import { GameProps, EGameStatus } from "./FlappyBird.mjs";
 
 export class TBait extends libSprite.TSprite {
@@ -9,15 +10,16 @@ export class TBait extends libSprite.TSprite {
         super(aSpriteCanvas, aSpriteInfo, aPosition);
         this.animateSpeed = 35;
         this.#speed = Math.ceil(Math.random() * 10) / 10 + 0.5;
-        this.#sinewave = new lib2d.TSineWave(100, 1);
+        const amplitude = Math.ceil(Math.random() * 3);
+        this.#sinewave = new lib2d.TSineWave(amplitude, 1);
         this.posY = this.#sinewave.value;
     }
 
     update(){
         if(GameProps.status === EGameStatus.playing){
-            this.translate(-this.#speed, 0);
+            this.translate(-this.#speed, this.#sinewave.value);
         } else {
-            this.translate(this.#speed / 2, 0);
+            this.translate(this.#speed / 2, this.#sinewave.value);
         }
     }
 }

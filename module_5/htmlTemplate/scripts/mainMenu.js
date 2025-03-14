ad574ccba10a8272a9c06a3172b1c7e3bf2f4258
JustRemi89 <remi-johansen@outlook.com>
2025-03-14 08:19:14 +0100

    added menuManager

--- module_5/htmlTemplate/scripts/mainMenu.js
+++ module_5/htmlTemplate/scripts/mainMenu.js
@@ -1,13 +1,29 @@
 "use strict";
 import TBootstrapComponent from "./bootstrapComponent.js";
 
-export default class TMainMenu extends TBootstrapComponent {
-    constructor() {
-        super();
-        this.attachShadow({mode: 'open'});
-    }
+class TMainMenu extends TBootstrapComponent {
+  constructor(){
+    super();
+    this.attachShadow({mode: "open"});
+  }
 
-    render() {
-        
+  render(){
+    const template = document.getElementById("main-menu-template");
+    const content = template.content.cloneNode(true);
+    this.shadowRoot.appendChild(content);
+    this.#createMenuManager();
+  }
+
+  #createMenuManager(){
+    const menuItems = this.shadowRoot.querySelectorAll("a[page-data]");
+    for(let i = 0; i < menuItems.length; i++){
+      const menuItem = menuItems[i];
+        //Vi må lage en ikke navngitt pilfunksjon pga. eventlytteren
+        menuItem.addEventListener("click",
+        () => {
+          console.log("Navigating to " + menuItem.getAttribute("page-data"));
+        });
     }
+  }
 }
+customElements.define("main-menu", TMainMenu);

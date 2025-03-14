1c77e53f00f167d3f38ca12aee5739acd0a5f6b7
JustRemi89 <remi-johansen@outlook.com>
2025-03-14 08:56:25 +0100

    added first css file

--- module_5/htmlTemplate/scripts/mainMenu.js
+++ module_5/htmlTemplate/scripts/mainMenu.js
@@ -2,28 +2,29 @@
 import TBootstrapComponent from "./bootstrapComponent.js";
 
 class TMainMenu extends TBootstrapComponent {
-  constructor(){
-    super();
-    this.attachShadow({mode: "open"});
-  }
-
-  render(){
-    const template = document.getElementById("main-menu-template");
-    const content = template.content.cloneNode(true);
-    this.shadowRoot.appendChild(content);
-    this.#createMenuManager();
-  }
-
-  #createMenuManager(){
-    const menuItems = this.shadowRoot.querySelectorAll("a[page-data]");
-    for(let i = 0; i < menuItems.length; i++){
-      const menuItem = menuItems[i];
-        //Vi må lage en ikke navngitt pilfunksjon pga. eventlytteren
-        menuItem.addEventListener("click",
-        () => {
-          console.log("Navigating to " + menuItem.getAttribute("page-data"));
-        });
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
     }
-  }
+
+    render(){
+        const template = document.getElementById("main-menu-template");
+        const content = template.content.cloneNode(true);
+        this.shadowRoot.appendChild(content);
+        this.#createMenuManager();
+    }
+
+    #createMenuManager(){
+        const menuItems = this.shadowRoot.querySelectorAll("a[page-data]");
+        for(let i = 0; i < menuItems.length; i++){
+        const menuItem = menuItems[i];
+            //Vi må lage en ikke navngitt pilfunksjon pga. eventlytteren
+            menuItem.addEventListener("click",() => this.#navigateToPage(menuItem));
+        }
+    } // End of #createMenuManager
+
+    #navigateToPage(aMenuItem){
+        console.log("Navigating to " + aMenuItem.getAttribute("page-data"));
+    }
 }
 customElements.define("main-menu", TMainMenu);

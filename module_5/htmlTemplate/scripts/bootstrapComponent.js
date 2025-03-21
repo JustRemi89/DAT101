f779cc77f9a5a9c62d875126425b7f5c32034168
JustRemi89 <remi-johansen@outlook.com>
2025-03-21 10:57:20 +0100

    added FontAwesome for icons

--- module_5/htmlTemplate/scripts/bootstrapComponent.js
+++ module_5/htmlTemplate/scripts/bootstrapComponent.js
@@ -7,6 +7,7 @@ export default class TBootstrapComponent extends HTMLElement {
 
     connectedCallback() {
         this.injectBootstrap();
+        this.injectFontAwesome();
         this.render();
     }
 
@@ -16,6 +17,13 @@ export default class TBootstrapComponent extends HTMLElement {
         linkElement.setAttribute('href', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
         linkElement.setAttribute('integrity', 'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH');
         linkElement.setAttribute('crossorigin', 'anonymous');
+        this.shadowRoot.appendChild(linkElement);
+    }
+
+    injectFontAwesome() {
+        const linkElement = document.createElement('link');
+        linkElement.setAttribute('rel', 'stylesheet');
+        linkElement.setAttribute('href', './fa/css/all.min.css');
         this.shadowRoot.appendChild(linkElement);
     }
 
